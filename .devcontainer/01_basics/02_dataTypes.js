008afa428607dac7897a0f78a1c5f94bd38c89d1
shubhambanugade <141548780+shubhambanugade@users.noreply.github.com>
2024-07-04 03:33:21 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code as newer version
+
+ //alert( 3 + 3)  // we are using nodejs, not using browser
+
+ console.log( 3
+    +3) //code readability should be high
+
+ console.log("shubham")
+
+
+ let name= "Shubham"
+ let age= 21
+ let isLoggedIn= false
+
+ // number => 2 to power 53
+ // bigint
+ // string => "" (should be use in double quotes)
+ // Boolean => true/false
+ // null =>stand alone value
+ // undefine =>
+ // symbol => unique
+
+
+ // object
+
+ console.log(typeof undefined)
