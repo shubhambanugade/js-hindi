946ff24c382bfb10098c66d4380dbc9b51d3d22a
shubhambanugade <141548780+shubhambanugade@users.noreply.github.com>
2024-07-05 03:56:14 +0000

    conversionOperation

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -3,3 +3,27 @@ let score = 33
 console.log(typeof score);
 console.log(typeof (score));
 
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true =>1 ; false => 0
+// null => NaN
+//undefined => NaN
+
+let isLoggedIn = 1
+
+let booleanLoggedIn = Boolean(isLoggedIn)
+console.log(booleanLoggedIn);
+
+// 1 =>true ; 0=>false
+// "" => false
+//"Shubham" =>true
+
+let someNumber = 44
+
+let stringNumber = String(someNumber)
+console.log(stringNumber );
+console.log(typeof stringNumber );
