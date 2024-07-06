0063ffbf9456c66861ec9d7345b987fe3fd6ca11
shubhambanugade <141548780+shubhambanugade@users.noreply.github.com>
2024-07-06 04:04:35 +0000

    comparison

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -11,7 +11,7 @@ console.log(valueInNumber);
 // "33abc" => NaN
 // true =>1 ; false => 0
 // null => NaN
-//undefined => NaN
+// undefined => NaN
 
 let isLoggedIn = 1
 
@@ -27,3 +27,38 @@ let someNumber = 44
 let stringNumber = String(someNumber)
 console.log(stringNumber );
 console.log(typeof stringNumber );
+
+
+
+//**************************************************Operations************************************************************ */
+
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2);
+
+let str1 = "Hello"
+let str2 = " Shubham"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log( "1" + 2);
+console.log( 1 + "2");
+console.log( 1 + "2" +2);
+console.log( 1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+let num1,num2,num3;
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
---
+++ .devcontainer/01_basics/04_comparison.js
@@ -0,0 +1,24 @@
+console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2==1);
+console.log(2!=1);
+
+
+console.log("2">1);
+console.log("02">1);
+
+
+console.log(null> 0);
+console.log(null== 0);
+console.log(null>= 0);
+
+console.log(undefined > 0);
+console.log(undefined < 0);
+console.log(undefined == 0);
+
+//mostly avoid that type of code , prefer clean code for coding
+
+// ===
+
+console.log("2" === 2);
