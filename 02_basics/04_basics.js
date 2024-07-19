d803c845c5de38dd0fdd1ba01d650ed31583153d
shubhambanugade <141548780+shubhambanugade@users.noreply.github.com>
2024-07-19 12:55:18 +0000

    new basics files

--- 02_basics/04_basics.js
+++ 02_basics/04_basics.js
@@ -7,7 +7,7 @@ tinderUser.id = "Abc12333334"
 tinderUser.name = "Sammy"
 tinderUser.isLoggedIn = false
 
-// console.log(tinderUser);
+console.log(tinderUser);
 
 const regularUser = {
     email : "Sammy@google.com",
@@ -19,10 +19,10 @@ const regularUser = {
     }
 } 
 
-// console.log(regularUser.fullname);
-// console.log(regularUser.fullname.userfullname);
-// console.log(regularUser.fullname.userfullname.firstname);
-// console.log(regularUser.fullname.userfullname.lastname);
+console.log(regularUser.fullname);
+console.log(regularUser.fullname.userfullname);
+console.log(regularUser.fullname.userfullname.firstname);
+console.log(regularUser.fullname.userfullname.lastname);
 
 
 const obj1 = { 1:"a" , 2:"b" } 
@@ -33,7 +33,7 @@ const obj4 = { 5:"e" , 6:"f" }
 // const obj3 = Object.assign({}, obj1 , obj2 , obj4)
 
 const obj3 = {...obj1 , ...obj2 , ...obj4}
-// console.log(obj3);
+console.log(obj3);
 
 const users = [
     {
@@ -51,12 +51,12 @@ const users = [
 ]
 
 users[1].email
-// console.log(tinderUser);
+console.log(tinderUser);
 
-// console.log(Object.keys(tinderUser));
-// console.log(Object.values(tinderUser));
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
 
-// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
 
 
 const course = {
@@ -69,7 +69,7 @@ const course = {
 
 const {courseinstructor :instructor} = course
 
-// console.log(courseinstructor);
+console.log(courseinstructor);
  console.log(instructor);
 
 //  {
