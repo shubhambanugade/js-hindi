5a7f685e06d2aa13e612ef24a1c60164b0a45344
shubhambanugade <141548780+shubhambanugade@users.noreply.github.com>
2024-07-27 12:16:23 +0000

    while and do-while loop

---
+++ 05_iterations/002_two.js
@@ -0,0 +1,24 @@
+
+// while loop
+
+let index = 0
+    while (index <= 10) {
+    // console.log(`the value of index is ${index}`);
+    index = index + 2
+}
+
+
+
+let score = 1
+do {
+    // console.log(`score is ${score}`);
+    score ++
+} while (score <= 10);
+
+
+let score1 = 11
+do {
+    console.log(`score is ${score1}`);
+    score1 ++
+} while (score1 <= 10);
+
